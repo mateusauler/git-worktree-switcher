c46bbf1fcbec637e8492c9accff60ac4da675f47
Mateus Auler <mateus@auler.dev>
2025-11-30 14:32:37 -0300

    Use `list` instead of `names` on completions

--- completions/wt.fish
+++ completions/wt.fish
@@ -1,4 +1,4 @@
 # AUTOCOMPLETION FOR FISH
 # Reference: https://fishshell.com/docs/current/completions.html
 
-complete -c wt -f -n '__fish_is_nth_token 1' -a "(wt names)"
+complete -c wt -f -n '__fish_is_nth_token 1' -a "(wt list)"
